2d2d196a0408c69e41f825027dc2f0b7ff511279
Allan Chepkoy <chepkoyallan@icloud.com>
2022-09-04 18:02:32 +0900

    Dashboard Base

---
+++ public/components/main-page/main-page.js
@@ -0,0 +1,9 @@
+
+import utilitiesMixin from '../mixins/utilities-mixin';
+import {PolymerElement} from '@polymer/polymer';
+
+export class MainPage extends utilitiesMixin(PolymerElement) {
+
+}
+
+window.customElements.define('main-page', MainPage);
---
+++ public/index.js
@@ -0,0 +1,5 @@
+// Entrypoint for Webpack
+import '@babel/polyfill';
+
+// import './components/main-page.js';
+import './components/main-page/main-page';
